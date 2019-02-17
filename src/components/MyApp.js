651004e73d4f6da60d1476f13566a4fb4420bf12
Riken Mehta <riken.mehta03@gmail.com>
2019-02-16 19:08:23 -0500

    [UPDATE]routing and proxy config

--- src/components/MyApp.js
+++ src/components/MyApp.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import grey from '@material-ui/core/colors/grey';
 
 import MyAppBar from "./MyAppBar";
 import LandingForm from "./LandingForm";
+import Login from "./Login";
 
 const theme = createMuiTheme({
     palette: {
@@ -16,10 +17,15 @@ const theme = createMuiTheme({
 
 export default (props) => {
     return (
-        <MuiThemeProvider theme={theme}>
-            <CssBaseline />
-            <MyAppBar />
-            <LandingForm />
-        </MuiThemeProvider>
+        <Router>
+            <MuiThemeProvider theme={theme}>
+                <CssBaseline />
+                <MyAppBar />
+                <Switch>
+                    <Route exact path="/" component={LandingForm} />
+                    <Route path="/logic" component={Login} />
+                </Switch>
+            </MuiThemeProvider>
+        </Router>
     );
 }
